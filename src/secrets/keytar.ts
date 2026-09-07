b0d1977b0c444b81d065e21edaaceaa0071f8f05
nothingtosayabouthis <voxxthisinnovember105@gmail.com>
2026-09-06 23:01:30 -0300

    fix: add Linux dependency installation and refactor keytar import method

--- src/secrets/keytar.ts
+++ src/secrets/keytar.ts
@@ -1,20 +1,23 @@
-import keytar from "keytar";
 import type { SecretStore } from "./types.js";
 
 export class KeytarStore implements SecretStore {
+  private async keytar() {
+    return await import("keytar");
+  }
+
   async set(service: string, account: string, secret: string) {
     try {
-      await keytar.setPassword(service, account, secret);
+      const keytar = await this.keytar();
+      await keytar.default.setPassword(service, account, secret);
     } catch {
-      throw new Error(
-        "Unable to store credentials securely."
-      );
+      throw new Error("Unable to store credentials securely.");
     }
   }
 
   async get(service: string, account: string) {
     try {
-      return await keytar.getPassword(service, account);
+      const keytar = await this.keytar();
+      return await keytar.default.getPassword(service, account);
     } catch {
       return null;
     }
@@ -22,7 +25,8 @@ export class KeytarStore implements SecretStore {
 
   async delete(service: string, account: string) {
     try {
-      return await keytar.deletePassword(service, account);
+      const keytar = await this.keytar();
+      return await keytar.default.deletePassword(service, account);
     } catch {
       return false;
     }
